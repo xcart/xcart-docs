b41fe556e130085ca6615ae43d9072cfd06e0ae9
Eugene Dementjev <daemos@x-cart.com>
2017-11-08 15:47:34 +0400

    [!] Fix CI config

--- get_targets.sh
+++ get_targets.sh
@@ -7,7 +7,6 @@ KB_EN_DIFF="$(git diff $REVISION HEAD --name-only _pages/kb_en)"
 KB_RU_DIFF="$(git diff $REVISION HEAD --name-only _pages/kb_ru)"
 MAIN_DIFF="$(git diff $REVISION HEAD --name-only !(_pages))"
 
-echo "$(ls -la)"
 if [ ! -z "$MAIN_DIFF" ]
   then
     touch "$1/devs_en"
