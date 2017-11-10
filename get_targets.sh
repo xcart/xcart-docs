6b30705b0b4e7c99af05455f8a96acd41f0a138d
Eugene Dementjev <daemos@x-cart.com>
2017-11-10 10:48:03 +0400

    [skip ci]

--- get_targets.sh
+++ get_targets.sh
@@ -17,7 +17,7 @@ DEVS_EN_DIFF="$(git diff $REVISION HEAD --name-only _pages/devs_en)"
 DEVS_RU_DIFF="$(git diff $REVISION HEAD --name-only _pages/devs_ru)"
 KB_EN_DIFF="$(git diff $REVISION HEAD --name-only _pages/kb_en)"
 KB_RU_DIFF="$(git diff $REVISION HEAD --name-only _pages/kb_ru)"
-MAIN_DIFF="$(git diff $REVISION HEAD --name-only !(_pages))"
+MAIN_DIFF="$(git diff $REVISION HEAD --name-only !(_pages|attachments))"
 
 if [ ! -z "$MAIN_DIFF" ]
   then
